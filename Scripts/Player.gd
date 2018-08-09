a8af966c8c7945b9aea631888ce0318fe455a8e3
Adam Brown <adamnathanielbrown@gmail.com>
2018-08-09 11:39:21 -0700

    I broke the controls before so I'm fixing them now
    wow i am bad

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,5 @@
 extends RigidBody2D
 
-# CONTROLS ####################################################################
-# TODO: Add Gamepad/Keyboard toggle
-var directionals = [0, 0, 0, 0] # Directional key states UDLR
-
 # SPRITE ######################################################################
 var spt
 var spriteSize = Vector2(0, 0)
@@ -20,10 +16,12 @@ var charge = 0
 func _ready():
 	spt = $Sprite
 	chargeBar = $ChargeBar
-	
+
 	spriteSize = spt.region_rect.size
 	print (spriteSize)
 	set_physics_process(true)
+
+	connect("body_entered", self, "bodyEntered")
 
 
 var prevDir = Vector2(0, 0) # Upon release, the current direction is almost always zero
@@ -32,60 +30,59 @@ func _physics_process(delta):
 
 	# DIRECTION
 	var dirx = 0
-	if Input.is_action_pressed("kb_left"): 
-		dirx = -1;  
-		directionals[2] = 1;
-	elif Input.is_action_pressed("kb_right"): 
-		dirx = 1; 
-		directionals[3] = 1;
-	else: 
+	if Input.is_action_pressed("kb_left"):
+		dirx = -1;
+	elif Input.is_action_pressed("kb_right"):
+		dirx = 1;
+	else:
 		dirx = 0;
-		directionals[2] = 0
-		directionals[3] = 0
-		
+
 	var diry = 0
-	if Input.is_action_pressed("kb_up"): 
-		diry = -1; 
-		directionals[0] = 1;
-	elif Input.is_action_pressed("kb_down"): 
-		diry = 1; 
-		directionals[1] = 1;
-	else: 
+	if Input.is_action_pressed("kb_up"):
+		diry = -1;
+	elif Input.is_action_pressed("kb_down"):
+		diry = 1;
+	else:
 		diry = 0;
-		directionals[0] = 0
-		directionals[1] = 0
-	
+
 	var direction = Vector2(dirx, diry)
 	spt.direction = direction
-	
+
 	# MOVEMENT
 	# - charging
 	if (Input.is_action_pressed("kb_direction")): # If any directional key is pressed
 		chargeTime += delta
 		if (charge < 1):
-			
 			# Linear-exponential switch
 			if (linear):
 				charge += chargeRate * delta
-				
 			else: # Exponential charging over time
-				charge += pow(chargeTime, exChargeRate)	
+				charge += pow(chargeTime, exChargeRate)
 		else:
 			charge = 1
-	
+
 	# - charge release
-	if (directionals == [0, 0, 0, 0]):
+	if (Input.is_action_just_released("kb_direction")):
+		var force = charge * maxForce
+
 		print ("DIRECTION: ", prevDir)
-		print ("RELEASED! with a force of ", charge * maxForce)
-		
+		print ("RELEASED! with a force of ", force)
+
 		chargeTime = 0
-		
+
 		if charge > 0.25: spt.flash();
-			
+
 		apply_impulse(Vector2(0, 0),
-			Vector2(prevDir.x * charge * maxForce, prevDir.y * charge * maxForce))
+		 Vector2(prevDir.x * charge * maxForce,
+		  prevDir.y * charge * maxForce))
 		charge = 0
+
 	# GUI
 	chargeBar.value = charge
-	
+
+	# FINALIZATION
 	prevDir = direction
+
+
+func bodyEntered (body):
+	print ("I've collided with ", str(body))
